5d6374cd8152b2f3f1923e42521baf106c906e99
joemer-solid <52687027+joemer-solid@users.noreply.github.com>
2019-07-09 11:43:05 -0400

    removed commented code and extraneous methods

--- Program.cs
+++ Program.cs
@@ -11,55 +11,49 @@ namespace fruitBasketStateMachine
         static void Main(string[] args)
         {
 
-            // Console.WriteLine(DayOfWeekResult("Sat", 23));
+            PrintToScreen(string.Format("Welcome to the Fruit Basket State Machine! {0}", Environment.NewLine));
 
-            string[] results = fibSequence(10);
-          
-            //PrintToScreen(string.Format("Welcome to the Fruit Basket State Machine! {0}", Environment.NewLine));
+            _fruitBasketService = new FruitBasketService();
 
-            //_fruitBasketService = new FruitBasketService();
+            PrintToScreen("Adding one apple...");
 
-            //PrintToScreen("Adding one apple...");
+            AddFruit(new Apple());
 
-            //AddFruit(new Apple());
+            PrintToScreen("Adding another apple...");
 
-            //PrintToScreen("Adding another apple...");
+            AddFruit(new Apple());
 
-            //AddFruit(new Apple());
+            ListAllFruitItems();
 
-            //ListAllFruitItems();
+            PrintToScreen("Adding a strawberry...");
 
-            //PrintToScreen("Adding a strawberry...");
+            AddFruit(new Strawberry());
 
-            //AddFruit(new Strawberry());
+            PrintToScreen("Adding an orange...");
 
-            //PrintToScreen("Adding an orange...");
+            AddFruit(new Orange());
 
-            //AddFruit(new Orange());
+            ListAllFruitItems();
 
-            //ListAllFruitItems();
+            PrintToScreen("Removing a pineapple...");
 
-            //PrintToScreen("Removing a pineapple...");
+            RemoveFruit(new Pineapple());
 
-            //RemoveFruit(new Pineapple());
+            PrintToScreen("Removing an apple...");
 
-            //PrintToScreen("Removing an apple...");
+            RemoveFruit(new Apple());
 
-            //RemoveFruit(new Apple());
+            ListAllFruitItems();
 
-            //ListAllFruitItems();
+            PrintToScreen("Adding a pineapple...");
 
-            //PrintToScreen("Adding a pineapple...");
+            AddFruit(new Pineapple());
 
-            //AddFruit(new Pineapple());
+            PrintToScreen("Adding an apple...");
 
-            //PrintToScreen("Adding an apple...");
+            AddFruit(new Apple());
 
-            //AddFruit(new Apple());
-
-            //ListAllFruitItems();
-
-
+            ListAllFruitItems();
 
 
 
@@ -115,70 +109,6 @@ namespace fruitBasketStateMachine
         {
             Console.WriteLine(message);
         }
-
-        private static string[] fibSequence(int fibSeqUenceRequest)
-        {
-            IList<string> fibList = new List<string> { "0", "1" };
-            Int64 getNextResult = 0;
-           
-
-            for (int i = 0; i < fibSeqUenceRequest - 2; i++)
-            {
-                
-                getNextResult = Convert.ToInt64(fibList[i]) + Convert.ToInt64(fibList[i + 1]);
-                fibList.Add(getNextResult.ToString());
-            }
-
-            return (string[])fibList.ToArray();
-        }
-
-
-        private static string DayOfWeekResult(string S, int K)
-        {
-
-
-            string dayOfWeekResult = string.Empty;
-            int startDOW = 0;
-            int dayOfWeekBuffer = 0;
-
-
-                switch(S)
-                {     
-
-
-                        case "Sun":
-                            startDOW = 0;
-                            break;
-
-                        case "Mon":
-                            startDOW = 1;
-                            break;
-
-                        case "Tue":
-                            startDOW = 2;
-                            break;
-
-                        case "Wed":
-                            startDOW = 3;
-                            break;
-
-                        case "Thur":
-                            startDOW = 4;
-                            break;
-
-                        case "Fri":
-                            startDOW = 5;
-                            break;
-
-                        case "Sat":
-                            startDOW = 6;
-                            break;
-                    }
-
-                    dayOfWeekBuffer = (startDOW + K) % 7;
-
-
-                    return ((DayOfWeek)dayOfWeekBuffer).ToString().Substring(0,3);
-        }
+       
     }
 }
